dc47f00a3e0664416863cc3366e7bf8f1bd8619c
ng3318 <ng3318@srmist.edu.in>
2026-03-09 11:30:41 +0530

    Refactored banner printing using String array and loop for UC4

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,34 +1,27 @@
 /**
  * OOPSBannerApp
- * UC3: Render OOPS as Banner using String.join()
+ * UC4: Render OOPS Banner using String Array and Loop
  *
  * @author YourName
- * @version 3.0
+ * @version 4.0
  */
 
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(String.join(" ",
-                " ***** ", " ***** ", " ****** ", " ***** "));
+        String[] banner = new String[7];
 
-        System.out.println(String.join(" ",
-                "*     *", "*     *", "*     *", "*     *"));
+        banner[0] = String.join(" ", " ***** ", " ***** ", " ****** ", " ***** ");
+        banner[1] = String.join(" ", "*     *", "*     *", "*     *", "*     *");
+        banner[2] = String.join(" ", "*     *", "*     *", "*     *", "*");
+        banner[3] = String.join(" ", "*     *", "*     *", "****** ", " ***** ");
+        banner[4] = String.join(" ", "*     *", "*     *", "*",        "     *");
+        banner[5] = String.join(" ", "*     *", "*     *", "*",        "     *");
+        banner[6] = String.join(" ", " ***** ", " ***** ", "*",       " ***** ");
 
-        System.out.println(String.join(" ",
-                "*     *", "*     *", "*     *", "*"));
-
-        System.out.println(String.join(" ",
-                "*     *", "*     *", "****** ", " ***** "));
-
-        System.out.println(String.join(" ",
-                "*     *", "*     *", "*", "     *"));
-
-        System.out.println(String.join(" ",
-                "*     *", "*     *", "*", "     *"));
-
-        System.out.println(String.join(" ",
-                " ***** ", " ***** ", "*", " ***** "));
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
